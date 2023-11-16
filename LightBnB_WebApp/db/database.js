e642edda2ccbdf82207ef2fdbc5534f20ec33a1a
TiffanyStPierre <stpierre.tiffany@gmail.com>
2023-11-16 07:41:22 -0700

    update addProperty function to update the database when a new property is created

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -59,16 +59,12 @@ const getUserWithId = function(id) {
 const addUser = function(user) {
   return pool
     .query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *`, [user.name, user.email, user.password])
-    .then(() => {
+    .then((result) => {
       return result.rows[0];
     })
     .catch((err) => {
       console.log(err.message);
     });
-  /*const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);*/
 };
 
 /// Reservations
@@ -177,10 +173,18 @@ const getAllProperties = function(options, limit = 10) {
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+
+  return pool
+    .query(`INSERT INTO properties 
+    (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms) 
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *`, 
+    [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms])
+    .then((result) => {
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 module.exports = {
